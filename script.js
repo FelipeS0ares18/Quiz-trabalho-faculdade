3f7e5de12d5ba0830c0c8a6ca514871d397bf81c
Felipe Soares <131181873+FelipeS0ares18@users.noreply.github.com>
2023-09-03 01:47:20 -0300

    Feat: Adição do botão inicial

--- script.js
+++ script.js
@@ -1,76 +1,81 @@
+import questions from "./questions.js";
+
 const question = document.querySelector(".question");
 const answers = document.querySelector(".answers");
 const spnQtd = document.querySelector(".spnQtd");
 const textFinish = document.querySelector(".finish span");
 const content = document.querySelector(".content");
 const contentFinish = document.querySelector(".finish");
-// Selecionando o botão Reiniciar pelo id
 const btnRestart = document.querySelector("#btnRestart");
-
-import questions from "./questions.js";
+const btnStart = document.querySelector("#btnStart"); // Adicionado botão "Começar"
 
 let currentIndex = 0;
 let questionsCorrect = 0;
 
 btnRestart.onclick = () => {
- content.style.display = "flex";
- contentFinish.style.display = "none";
+  content.style.display = "flex";
+  contentFinish.style.display = "none";
+  currentIndex = 0;
+  questionsCorrect = 0;
+  loadQuestion();
+};
 
- currentIndex = 0;
- questionsCorrect = 0;
- loadQuestion();
+// Função para começar o quiz quando o botão "Começar" é clicado
+btnStart.onclick = () => {
+  content.style.display = "flex";
+  btnStart.style.display = "none"; // Oculta o botão "Começar"
+  loadQuestion();
 };
 
 function nextQuestion(e) {
- if (e.target.getAttribute("data-correct") === "true") {
- questionsCorrect++;
- }
+  if (e.target.getAttribute("data-correct") === "true") {
+    questionsCorrect++;
+  }
 
- if (currentIndex < questions.length - 1) {
- currentIndex++;
- loadQuestion();
- } else {
- finish();
- }
+  if (currentIndex < questions.length - 1) {
+    currentIndex++;
+    loadQuestion();
+  } else {
+    finish();
+  }
 }
 
 function finish() {
- textFinish.innerHTML = `você acertou ${questionsCorrect} de ${questions.length}`;
- content.style.display = "none";
- contentFinish.style.display = "flex";
+  textFinish.innerHTML = `Você acertou ${questionsCorrect} de ${questions.length}`;
+  content.style.display = "none";
+  contentFinish.style.display = "flex";
 }
 
 function loadQuestion() {
- spnQtd.innerHTML = `${currentIndex + 1}/${questions.length}`;
- const item = questions[currentIndex];
- answers.innerHTML = "";
- question.innerHTML = item.question;
+  spnQtd.innerHTML = `${currentIndex + 1}/${questions.length}`;
+  const item = questions[currentIndex];
+  answers.innerHTML = "";
+  question.innerHTML = item.question;
 
- item.answers.forEach((answer) => {
- const div = document.createElement("div");
+  item.answers.forEach((answer) => {
+    const div = document.createElement("div");
 
- div.innerHTML = `
- <button class="answer" data-correct="${answer.correct}">
- ${answer.option}
- </button>
- `;
+    div.innerHTML = `
+      <button class="answer" data-correct="${answer.correct}">
+        ${answer.option}
+      </button>
+    `;
 
- answers.appendChild(div);
- });
+    answers.appendChild(div);
+  });
 
- document.querySelectorAll(".answer").forEach((item) => {
- item.addEventListener("click", nextQuestion);
- });
+  document.querySelectorAll(".answer").forEach((item) => {
+    item.addEventListener("click", nextQuestion);
+  });
 }
 
-loadQuestion();
 
 // Código para o modal
 // Obtém o modal
 var modal = document.getElementById("myModal");
 
 // Obtém o botão que abre o modal
-var btn = document.getElementById("myBtn");
+var btn = document.getElementById("myGabarito");
 
 // Obtém o elemento <span> que fecha o modal
 var span = document.getElementsByClassName("close")[0];
